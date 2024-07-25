a9621e97950667e45c27d9af07fef61a8c67a631
Pedro Rabitti <63475606+hortapimo@users.noreply.github.com>
2024-07-25 20:41:28 -0300

    agregue input mouse

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,14 +1,23 @@
 #include "controller.hpp"
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
 
 Controller::Controller(Model& model, View& view) : model(model), view(view) {}
 
+void Controller::mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+                // Imprime las coordenadas guardadas
+        std::cout << "Mouse position: x = " << savedXPos << ", y = " << savedYPos << std::endl;
+    }
+}
 
+void Controller::cursor_pos_callback(GLFWwindow* window, double xpos, double ypos) {
+    // Guarda las coordenadas del mouse
+    
+    savedXPos =xpos;
+    savedYPos = ypos;
+}
 
 void Controller::run() {
-
+//inicializacion de glfw y de la ventana
      if (!glfwInit()) {
         return;
     }
@@ -18,18 +27,23 @@ void Controller::run() {
         return;
     }
     glfwMakeContextCurrent(window);
-
-
     if(glewInit() != GLEW_OK){
         std::cout <<"error glew!\n";
     }
     
+//registro de funciones
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    glfwSetCursorPosCallback(window, cursor_pos_callback);
+
+//loop principal
     while (!glfwWindowShouldClose(window)) {
+
+
+
         view.render(model.getVertices());
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
     glfwDestroyWindow(window);
     glfwTerminate();
 }
--- src/controller.hpp
+++ src/controller.hpp
@@ -1,17 +1,28 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
 #include "model.hpp"
 #include "view.hpp"
+
 
 class Controller {
 public:
     Controller(Model& model, View& view);
+    static void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+    static void cursor_pos_callback(GLFWwindow* window, double xpos, double ypos);
     void run();
+    static inline float savedXPos = 0.0;
+    static inline float savedYPos = 0.0;
 
 private:
     Model& model;
     View& view;
 };
+
 
 #endif // CONTROLLER_H
