5d22a526aaac88928b837e7a91bd8e8f6ff364af
Pedro Rabitti <63475606+hortapimo@users.noreply.github.com>
2024-07-28 10:14:26 -0300

    pezando

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,10 +3,7 @@
 Controller::Controller(Model& model, View& view) : model(model), view(view) {}
 
 void Controller::mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
-    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-                // Imprime las coordenadas guardadas
-        std::cout << "Mouse position: x = " << savedXPos << ", y = " << savedYPos << std::endl;
-    }
+
 }
 
 void Controller::cursor_pos_callback(GLFWwindow* window, double xpos, double ypos) {
@@ -17,33 +14,5 @@ void Controller::cursor_pos_callback(GLFWwindow* window, double xpos, double ypo
 }
 
 void Controller::run() {
-//inicializacion de glfw y de la ventana
-     if (!glfwInit()) {
-        return;
-    }
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Ceibo", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return;
-    }
-    glfwMakeContextCurrent(window);
-    if(glewInit() != GLEW_OK){
-        std::cout <<"error glew!\n";
-    }
-    
-//registro de funciones
-    glfwSetMouseButtonCallback(window, mouseButtonCallback);
-    glfwSetCursorPosCallback(window, cursor_pos_callback);
 
-//loop principal
-    while (!glfwWindowShouldClose(window)) {
-
-
-
-        view.render(model.getVertices());
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwDestroyWindow(window);
-    glfwTerminate();
 }
