78aa5c7a678be848d9ad90f2a2b43585b672952b
Pedro Rabitti <63475606+hortapimo@users.noreply.github.com>
2024-07-28 20:15:29 -0300

    pude poner el sfml!

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,19 +3,23 @@
 Controller::Controller(Model& model, View& view) : model(model), view(view) {}
 
 void Controller::run() {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
 
-    // run the program as long as the window is open
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
     }
 
 }
--- src/controller.hpp
+++ src/controller.hpp
@@ -3,6 +3,7 @@
 
 
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 #include <iostream>
 
