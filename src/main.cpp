3c99d01e8510afa866110fcd7fcb98b159ca0b82
Pedro Rabitti <63475606+hortapimo@users.noreply.github.com>
2024-08-06 19:39:16 -0300

    ff

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,32 @@ int main() {
     View view;
     Controller controller(model, view);
 
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+
+    while (window.isOpen())
+    {
+
+        controller.tomarEvento();
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+
+
+*/
+
     controller.run();
+
+
 
     return 0;
 }
